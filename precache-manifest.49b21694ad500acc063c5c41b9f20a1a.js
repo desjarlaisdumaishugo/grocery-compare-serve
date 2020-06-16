dda794491078cb86184fd945f4dbcbb8df3c0911
Hugo Desjarlais-Dumais <desjarlaisdumaishugo@gmail.com>
2020-06-15 21:31:47 -0400

    initial commit

---
+++ precache-manifest.49b21694ad500acc063c5c41b9f20a1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89e831200c3f22bd317c8ca409bc295",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4309b9f9cf8039fa43dd",
+    "url": "/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "739dc142df9b04f77838",
+    "url": "/static/css/main.14e1f5f0.chunk.css"
+  },
+  {
+    "revision": "4309b9f9cf8039fa43dd",
+    "url": "/static/js/2.77e4d255.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.77e4d255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "739dc142df9b04f77838",
+    "url": "/static/js/main.6417ec0e.chunk.js"
+  },
+  {
+    "revision": "5710096fc0703ad1ce64",
+    "url": "/static/js/runtime-main.98eaffb0.js"
+  }
+]);
